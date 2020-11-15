63bee11a0b9804383a6f3c928ee5f17170f20e08
swaggerloaf <swaggerloaf@gmail.com>
2020-11-14 20:39:44 -0500

    added TodoItem

---
+++ TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Net5ToDo.Core
+{
+  public class TodoItem
+  {
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+  }
+}
